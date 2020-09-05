5b44c564e9f6700fb7aa0c73c7de6c572653f079
hermanthegerman2 <hermanthegerman2@users.noreply.github.com>
2020-09-05 22:54:27 +0200

    Update settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -18,7 +18,7 @@ class blz_eventwoo_settings_tab {
      * @return array
      */
     public static function settings_link( $links ) {
-        $settings_link = '<a href="admin.php?page=wc-settings&tab=settings_tab_blz_eventwoo">' . __( 'Settings', 'eventwoo' ) . '</a>';
+        $settings_link = '<a href="admin.php?page=wc-settings&tab=settings_tab_blz_eventwoo">' . __( 'Einstellungen', 'eventwoo' ) . '</a>';
         array_push( $links, $settings_link );
         return $links;
     }
@@ -30,7 +30,7 @@ class blz_eventwoo_settings_tab {
      * @return array
      */
     public static function add_settings_tab( $settings_tabs ) {
-        $settings_tabs['settings_tab_blz_eventwoo'] = __( 'Events', 'eventwoo' );
+        $settings_tabs['settings_tab_blz_eventwoo'] = __( 'Veranstaltungen', 'eventwoo' );
         return $settings_tabs;
     }
 
@@ -65,30 +65,30 @@ class blz_eventwoo_settings_tab {
     public static function get_settings() {
         $settings = array(
             'blz_eventwoo_booking_recommendation' => array(
-                'name' => __( 'Recommendations', 'eventwoo' ),
+                'name' => __( 'Empfehlungen', 'eventwoo' ),
                 'type' => 'title',
-                'desc' => __( 'Events payments currently requires that users are logged in so we recommend setting the Allow Guest Bookings (in Events -> Settings -> Bookings -> General Options) to No. <br/>We also recommend that you allow users to register (in Settings -> General -> Membership).', 'eventwoo' ),
+                'desc' => __( 'Für die Bezahlung von Veranstaltungen ist es derzeit erforderlich, dass die Benutzer eingeloggt sind. Wir empfehlen daher, die Option Gastbuchungen zulassen (in Veranstaltungen -> Einstellungen -> Buchungen -> Allgemeine Optionen) auf Nr. <br/> Wir empfehlen außerdem, die Registrierung von Benutzern zuzulassen (in Einstellungen -> Allgemein -> Mitgliedschaft).', 'eventwoo' ),
                 'id'   => 'blz_eventwoo_booking_recommendation',
             ),
             'section_title_events_page' => array(
-                'name'     => __( 'Events Page Settings', 'eventwoo' ),
+                'name'     => __( 'Einstellungen der Ereignisseite', 'eventwoo' ),
                 'type'     => 'title',
                 'desc'     => '',
                 'id'       => 'blz_eventwoo_title'
             ),
             'blz_eventwoo_product_added_message' => array(
-                'name' => __( 'Product added message', 'eventwoo' ),
+                'name' => __( 'Nachricht zum Produkt hinzugefügt', 'eventwoo' ),
                 'type' => 'textarea',
-                'desc' => __( 'The message displayed to the user when they make a booking on the event page', 'eventwoo' ),
+                'desc' => __( 'Die Nachricht, die dem Benutzer angezeigt wird, wenn er eine Buchung auf der Veranstaltungsseite vornimmt', 'eventwoo' ),
                 'id'   => 'blz_eventwoo_product_added_message',
-                'default' => 'You have a pending booking for this event, we have added an event booking item to your shopping basket',
+                'default' => 'Sie haben eine ausstehende Buchung für diese Veranstaltung, wir haben Ihrem Warenkorb eine Veranstaltungsbuchung hinzugefügt.',
             ),
             'blz_eventwoo_product_added_link_to_cart' => array(
-                'name' => __( 'Link to cart message', 'eventwoo' ),
+                'name' => __( 'Link zur Warenkorb-Nachricht', 'eventwoo' ),
                 'type' => 'textarea',
-                'desc' => __( 'The text displayed on the cart link when they make a booking on the event page', 'eventwoo' ),
+                'desc' => __( 'Der Text, der auf dem Warenkorb-Link angezeigt wird, wenn sie eine Buchung auf der Veranstaltungsseite vornehmen', 'eventwoo' ),
                 'id'   => 'blz_eventwoo_product_added_link_to_cart',
-                'default' => 'please proceed to the checkout when you are ready.',
+                'default' => 'Bitte gehen Sie zur Kasse, wenn Sie bereit sind.',
             ),
             'section_end_events_page' => array(
                 'type' => 'sectionend',
@@ -96,21 +96,21 @@ class blz_eventwoo_settings_tab {
             ),
 
             'section_title_events_cart' => array(
-                'name'     => __( 'Cart Settings', 'eventwoo' ),
+                'name'     => __( 'Warenkorb-Einstellungen', 'eventwoo' ),
                 'type'     => 'title',
                 'desc'     => '',
                 'id'       => 'blz_eventwoo_events_cart_title'
             ),
             'blz_eventwoo_hide_product_name_in_cart' => array(
-                'name' => __( 'Hide product name in cart', 'eventwoo' ),
+                'name' => __( 'Produktname im Warenkorb ausblenden', 'eventwoo' ),
                 'type' => 'checkbox',
-                'desc' => __( 'This hides the product name in the cart page and mini cart.', 'eventwoo' ),
+                'desc' => __( 'Dadurch wird der Produktname auf der Warenkorbseite und im Miniwagen ausgeblendet.', 'eventwoo' ),
                 'id'   => 'blz_eventwoo_hide_product_name_in_cart'
             ),
             'blz_eventwoo_disable_spaces_if_logged_out' => array(
-                'name' => __( 'Disable the Spaces dropdowns if logged out', 'eventwoo' ),
+                'name' => __( 'Deaktivieren Sie die Dropdown-Listenfelder Spaces, wenn Sie abgemeldet sind', 'eventwoo' ),
                 'type' => 'checkbox',
-                'desc' => __( 'This disables the Spaces dropdown selectors if the vsitior is not logged in.', 'eventwoo' ),
+                'desc' => __( 'Dadurch werden die Dropdown-Listenfelder Spaces deaktiviert, wenn der Besucher nicht angemeldet ist.', 'eventwoo' ),
                 'id'   => 'blz_eventwoo_disable_spaces_if_logged_out'
             ),
             'section_end_events_cart' => array(
@@ -119,15 +119,15 @@ class blz_eventwoo_settings_tab {
             ),
 
             'section_title_events_order' => array(
-                'name'     => __( 'Order Settings', 'eventwoo' ),
+                'name'     => __( 'Auftrags-Einstellungen', 'eventwoo' ),
                 'type'     => 'title',
                 'desc'     => '',
                 'id'       => 'blz_eventwoo_order_title'
             ),
             'blz_eventwoo_set_event_orders_completed' => array(
-                'name' => __( 'Set orders to completed when paid', 'eventwoo' ),
+                'name' => __( 'Aufträge auf erledigt setzen, wenn bezahlt', 'eventwoo' ),
                 'type' => 'checkbox',
-                'desc' => __( 'This bypasses the Processing order status. Not recommended if you are also selling physical products.', 'eventwoo' ),
+                'desc' => __( 'Dadurch wird der Status Verarbeitungsauftrag umgangen. Nicht empfehlenswert, wenn Sie auch physische Produkte verkaufen.', 'eventwoo' ),
                 'id'   => 'blz_eventwoo_set_event_orders_completed'
             ),
             'section_title_end_events_order' => array(
@@ -136,23 +136,23 @@ class blz_eventwoo_settings_tab {
             ),
 
             'blz_eventwoo_registration_section' => array(
-                'name'     => __( 'Registration Message', 'eventwoo' ),
+                'name'     => __( 'Nachricht zur Registrierung', 'eventwoo' ),
                 'type'     => 'title',
                 'desc'     => '',
                 'id'       => 'blz_eventwoo_registration_section'
             ),
             'blz_eventwoo_display_registered_message_on_event_page' => array(
-                'name' => __( 'Display registration message', 'eventwoo' ),
+                'name' => __( 'Registrierungsnachricht anzeigen', 'eventwoo' ),
                 'type' => 'checkbox',
-                'desc' => __( 'Display the Account Created message on the Event page when just registered.', 'eventwoo' ),
+                'desc' => __( 'Zeigen Sie die Meldung "Konto erstellt" auf der Ereignisseite an, wenn Sie sich gerade registriert haben.', 'eventwoo' ),
                 'id'   => 'blz_eventwoo_display_registered_message_on_event_page'
             ),
             'blz_eventwoo_registered_message' => array(
-                'name' => __( 'Registered message', 'eventwoo' ),
+                'name' => __( 'Registrierte Nachricht', 'eventwoo' ),
                 'type' => 'textarea',
-                'desc' => __( 'The message displayed to when the user on the Event page has just registered.', 'eventwoo' ),
+                'desc' => __( 'Die Meldung, die angezeigt wird, wenn sich der Benutzer auf der Ereignisseite gerade registriert hat.', 'eventwoo' ),
                 'id'   => 'blz_eventwoo_registered_message',
-                'default' => 'Your account has been created and we\'ve logged you in. We\'ve sent you an email with your username and password.',
+                'default' => 'Ihr Konto wurde erstellt und wir haben Sie eingeloggt. Wir haben Ihnen eine E-Mail mit Ihrem Benutzernamen und Passwort geschickt.',
             ),
 
             'section_end_registration' => array(
